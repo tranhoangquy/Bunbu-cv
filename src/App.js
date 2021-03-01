a10ba81d55e39e32cc714633feb1034b760d954c
Hoang Quy <tranhoangquy98@gmail.com>
2021-03-01 13:39:05 +0700

    change-forgotPassword-ui

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import './App.css'
 import routes from './Router'
 import ForgotPassword from './components/forgotPassword/forgotPassword'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import axios from 'axios'
 function App() {
-    return (
-        <div className="App">
-            <Router>
-                <Switch>
-                    <Route
-                        exact
-                        path="/forgotPassword"
-                        component={ForgotPassword}
-                    ></Route>
-                </Switch>
-            </Router>
-        </div>
-    )
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/forgotPassword"
+            component={ForgotPassword}
+          ></Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App
--- src/components/forgotPassword/forgotPassword.js
+++ src/components/forgotPassword/forgotPassword.js
@@ -1,25 +1,27 @@
 import React, { useState } from 'react'
 import logo from '../../assets/bunbu.png'
 export default function ForgotPassword(props) {
-    return (
-        <>
-            <img src={logo} style={{ width: '151px', marginLeft: '42%' }} />
-            <form style={{ maxWidth: '500px', margin: '0 auto' }}>
-                <h3>Forgot your password</h3>
-                <div className="form-group">
-                    <label>You Email address</label>
-                    <input
-                        type="email"
-                        placeholder="Enter email"
-                        className="form-control"
-                        required="true"
-                    />
-                </div>
+  const [email, setEmail] = useState('')
+  return (
+    <>
+      <img src={logo} style={{ width: '151px', marginLeft: '42%' }} />
+      <form style={{ maxWidth: '500px', margin: '0 auto' }}>
+        <h3>Forgot your password</h3>
+        <div className="form-group">
+          <label>You Email address</label>
+          <input
+            type="email"
+            placeholder="Enter email"
+            className="form-control"
+            required="true"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-                <button type="submit" className="btn btn-primary btn-block">
-                    Send me email
-                </button>
-            </form>
-        </>
-    )
+        <button type="submit" className="btn btn-primary btn-block">
+          Send me email
+        </button>
+      </form>
+    </>
+  )
 }
