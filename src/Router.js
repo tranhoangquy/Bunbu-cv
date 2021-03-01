7717b7c41fd975545e3d6d6035f0cfbe72123781
Hoang Quy <tranhoangquy98@gmail.com>
2021-03-01 13:21:40 +0700

    change-login-ui

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,6 @@
 import React from 'react'
-import HomeAdmin from './components/admin/homeAdmin'
-import Template from './components/admin/template/template'
+
 const route = [
-  {
-    path: '/admin',
-    exact: false,
-    main: () => <HomeAdmin />,
-  },
-  {
-    path: '/template',
-    exact: false,
-    main: () => <Template />,
-  },
+  
 ]
 export default route
