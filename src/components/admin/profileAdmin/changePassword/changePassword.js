e789c8caecfe505f3089d92eb7508a2f6651d61b
tranhoangquy <tranhoangquy98@gmail.com>
2021-03-29 15:21:22 +0700

    change Password

--- src/components/admin/profileAdmin/changePassword/changePassword.js
+++ src/components/admin/profileAdmin/changePassword/changePassword.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col, Button, Form } from 'react-bootstrap'
+import { password } from '../../../../pattern'
 import './changePassword.css'
 export default function ChangePassword(props) {
+  const [valueCurrentPw,setValueCurrentPw]=useState('')
+  const [valueNewPw,setValueNewPw]=useState('')
+  const [valueConfirmPw,setValueConfirmPw]=useState('')
+  const [displayerr,setDisplayerr] = useState(false)
+  const handlingSubmit =(e)=>{
+    e.preventDefault()
+   {valueConfirmPw != valueNewPw ? setDisplayerr(true):setDisplayerr(false)}
+  }
   return (
     <Container
       className="container-changepassword"
@@ -12,7 +21,7 @@ export default function ChangePassword(props) {
       <Row>
         <p className="changepassword-profile">ChangePassword</p>
       </Row>
-      <Form className="form-changepassword">
+      <Form className="form-changepassword" onSubmit={handlingSubmit}>
         <Row>
           <Col sm={6}>
             <p>Current Password</p>
@@ -21,7 +30,12 @@ export default function ChangePassword(props) {
             <Form.Control
               text="password"
               type="password"
+              name="currentPassword"
               placeholder="Current password"
+              pattern={password}
+              title="Password minimum 8 characters, at least one letter and a number"
+              required={true}
+              onChange={(e)=>{setValueCurrentPw(e.target.value)}}
             />
           </Col>
         </Row>
@@ -31,9 +45,13 @@ export default function ChangePassword(props) {
           </Col>
           <Col sm={4}>
             <Form.Control
-              name="newpassword"
+              name="newPassword"
               type="password"
               placeholder="New password"
+              pattern={password}
+              title="Password minimum 8 characters, at least one letter and a number"
+              required={true}
+              onChange={(e)=>{setValueNewPw(e.target.value)}}
             />
           </Col>
         </Row>
@@ -41,13 +59,18 @@ export default function ChangePassword(props) {
           <Col sm={6}>
             <p>New Password Comfirmation</p>
           </Col>
-          <Col sm={4}>
+          <Col sm={5}>
             <Form.Control
-              name="newpasswordconfirm"
+              name="confirmPassword"
               type="password"
               placeholder="New password confirmation"
+              required={true}
+              onChange={(e)=>{setValueConfirmPw(e.target.value  )}}
             />
+            { displayerr ? <a className="err">Confirm password do not coincide New pasword</a>: null }
+           
           </Col>
+          
         </Row>
         <Row>
           <Button type="submit">Save</Button>
