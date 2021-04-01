1b971edb8e544cf783a406701d75d48a16b40e46
tranhoangquy <tranhoangquy98@gmail.com>
2021-04-01 08:49:16 +0700

    indent2

--- src/reducer/staffReducer.js
+++ src/reducer/staffReducer.js
@@ -8,26 +8,26 @@ const initialState = {
 
 const staffReducer = (state= initialState, actions) =>{
 switch (actions.type){
-	case PAGE_REQUEST:
-		return {
-		...state,
-		requesting: true,
-		}
-	case CREATE_CV_SUCCESS:
-		return{
-			...state,
-			requesting: false,
-			data:actions.data
-		}
-	case CREATE_CV_ERROR:
-		return{
-			...state,
-			requesting: false,
-			success: false,
-			message: actions.message,
-		}
-	default:
-		return state
+case PAGE_REQUEST:
+  return {
+  ...state,
+  requesting: true,
+  }
+case CREATE_CV_SUCCESS:
+  return{
+    ...state,
+    requesting: false,
+    data:actions.data
+  }
+case CREATE_CV_ERROR:
+  return{
+    ...state,
+    requesting: false,
+    success: false,
+    message: actions.message,
+  }
+default:
+  return state
 }
 }
 export default staffReducer
